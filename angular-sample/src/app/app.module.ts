f8aefd19afb096efa7b502f756cbc40ad09236cd
Dayananda <kdayananda4783@gmail.com>
2018-05-18 14:42:10 -0500

    router outlet

--- angular-sample/src/app/app.module.ts
+++ angular-sample/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
@@ -9,17 +10,25 @@ import { TabsComponent } from './tabs/tabs.component';
 import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
 import { CreateComponentComponent } from './create-component/create-component.component';
+import { HeaderComponent } from './header/header.component';
 
+const routes = [
+  {path: '', component: TabsComponent},
+  {path: 'new-character', component: CreateComponentComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ItemComponent,
     ListComponent,
     TabsComponent,
-    CreateComponentComponent
+    CreateComponentComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
